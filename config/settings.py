8e25e733beb50622ca345143048df534f8535a15
Likitha-projects <likitharani2005@gmail.com>
2026-09-12 21:59:37 +0530

    fix allowed hosts for vercel

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-g5lv5!ps*ek&mt96i8zojj6oyigfe^!#)c4y#(1z=!awfg@%4^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
