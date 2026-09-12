ae0d52019d524299a3d6fbe92c34ecc307372bb3
Likitha-projects <likitharani2005@gmail.com>
2026-09-12 21:50:12 +0530

    fix PEP 668 pip install block

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 echo "Building project..."
-pip3 install -r requirements.txt
+python3 -m pip install -r requirements.txt --break-system-packages
 python3 manage.py collectstatic --noinput
+echo "Build complete."
 echo "Build complete."
