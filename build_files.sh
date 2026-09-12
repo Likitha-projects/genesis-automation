5756af9a64cfe9a5fdc301854f6773bbba00e4b5
Likitha-projects <likitharani2005@gmail.com>
2026-09-12 21:29:38 +0530

    updated vercel build files

--- build_files.sh
+++ build_files.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-pip install -r requirements.txt
-python manage.py collectstatic --noinput
+echo "Building project..."
+python3.9 -m pip install -r requirements.txt
+python3.9 manage.py collectstatic --noinput
+echo "Build complete."
